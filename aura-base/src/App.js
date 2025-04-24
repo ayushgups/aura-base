c4734ac83248e188e49738e18cca9d97fa498691
Krish Arora <arorakrish3000@gmail.com>
2025-04-23 23:25:54 -0700

    added the submit page

--- aura-base/src/App.js
+++ aura-base/src/App.js
@@ -1,48 +1,61 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import Sidebar from './components/Sidebar';
 import AuraLeaderboard from './components/AuraLeaderboard';
 import StatsCards from './components/StatsCards';
 import RecentActivity from './components/RecentActivity';
 import ReviewNotifications from './components/ReviewNotifications';
+import Submit from './pages/Submit';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+function HomePage() {
+  return (
+    <Container fluid className="py-4">
+      <div className="d-flex justify-content-between align-items-center mb-4">
+        <h1>Overview</h1>
+        <div className="team-selector">
+          The Kava Kangaroos
+        </div>
+      </div>
+      
+      {/* Top row with Leaderboard and Stats */}
+      <Row className="g-4 mb-4">
+        <Col lg={8}>
+          <AuraLeaderboard />
+        </Col>
+        <Col lg={4}>
+          <StatsCards />
+        </Col>
+      </Row>
+
+      {/* Bottom row with Activity and Notifications */}
+      <Row className="g-4">
+        <Col lg={8}>
+          <RecentActivity />
+        </Col>
+        <Col lg={4}>
+          <ReviewNotifications />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
 function App() {
   return (
-    <div className="app">
-      <Sidebar />
-      <div className="main-content">
-        <Container fluid className="py-4">
-          <div className="d-flex justify-content-between align-items-center mb-4">
-            <h1>Overview</h1>
-            <div className="team-selector">
-              The Kava Kangaroos
-            </div>
-          </div>
-          
-          {/* Top row with Leaderboard and Stats */}
-          <Row className="g-4 mb-4">
-            <Col lg={8}>
-              <AuraLeaderboard />
-            </Col>
-            <Col lg={4}>
-              <StatsCards />
-            </Col>
-          </Row>
-
-          {/* Bottom row with Activity and Notifications */}
-          <Row className="g-4">
-            <Col lg={8}>
-              <RecentActivity />
-            </Col>
-            <Col lg={4}>
-              <ReviewNotifications />
-            </Col>
-          </Row>
-        </Container>
+    <Router>
+      <div className="app">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/submit" element={<Submit />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- aura-base/src/components/Sidebar.js
+++ aura-base/src/components/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 import { FaHome, FaUpload, FaHistory } from 'react-icons/fa';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
     <div className="sidebar bg-dark text-white">
       <div className="sidebar-brand mb-4 p-3">
@@ -11,13 +14,25 @@ const Sidebar = () => {
         <span className="ms-2 h4">AuraBase</span>
       </div>
       <Nav className="flex-column">
-        <Nav.Link className="text-white">
+        <Nav.Link 
+          as={Link} 
+          to="/" 
+          className={`text-white ${location.pathname === '/' ? 'active' : ''}`}
+        >
           <FaHome className="me-2" /> Home
         </Nav.Link>
-        <Nav.Link className="text-white">
+        <Nav.Link 
+          as={Link} 
+          to="/submit" 
+          className={`text-white ${location.pathname === '/submit' ? 'active' : ''}`}
+        >
           <FaUpload className="me-2" /> Submit
         </Nav.Link>
-        <Nav.Link className="text-white">
+        <Nav.Link 
+          as={Link} 
+          to="/history" 
+          className={`text-white ${location.pathname === '/history' ? 'active' : ''}`}
+        >
           <FaHistory className="me-2" /> History
         </Nav.Link>
       </Nav>
